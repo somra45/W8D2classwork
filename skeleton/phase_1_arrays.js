8599e70974d70a3856e40ef32dced65ecb622af4
root <somra45@gmail.com>
2023-04-25 14:13:58 -0400

    finish phase 1

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -8,5 +8,41 @@ Array.prototype.uniq = function(){
     return new_arr 
 };
 
+Array.prototype.two_sum = function(){
+    let new_arr = []
+    const arr = this
+    arr.forEach(function(ele1, i){
+        arr.forEach(function(ele2, j){
+            if (ele1 + ele2 === 0 && j > i) {
+                new_arr.push([i, j])
+            }
+        })
+    })
+    return new_arr
+};
 
-// console.log([1,2,2,3,3,3].uniq());
+Array.prototype.transpose = function(){
+    let height = this.length
+    let width = this[0].length
+    const arr = this
+    let matrix = new Array(height) 
+        for (let i = 0; i < width; i++) {
+            matrix[i] = new Array
+        };
+    
+    for (let i = 0; i < width; i++) {
+        for (let j = 0; j < height; j++) {
+            matrix[i][j] = arr[j][i]
+        };
+    };
+    return matrix   
+};
+
+const matrix = [
+    [1, 2],
+    [4, 5],
+    [7, 8]
+  ];
+
+console.log(matrix.transpose());
+
